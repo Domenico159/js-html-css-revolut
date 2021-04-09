fc629bb74fc7e6834c0110653f9c2704afa02da2
Domenico159 <domenico.zummo@gmail.com>
2021-04-09 20:12:17 +0200

    Project Completed beta 1.0

---
+++ js/script.js
@@ -0,0 +1,100 @@
+
+
+$(document).ready( function() {
+
+    // Reference
+var allLink = $('header .nav-right > ul > li');
+
+var dropBig = $('header .nav-right ul.dropdown-big');
+
+var drop1 = $('header .nav-right ul.dropdown-small.drop-1');
+
+var drop2 = $('header .nav-right ul.dropdown-small.drop-2');
+
+var drop3 = $('header .nav-right ul.dropdown-small.drop-3');
+
+var itemActive =$('header .nav-right ul.active') ;
+
+
+
+
+console.log(allLink);
+
+
+  allLink.click( function() {
+
+    var thisElement = $(this);
+
+    var indexElement = thisElement.index() ;
+
+    console.log(indexElement);
+
+    
+
+
+
+    switch (indexElement) {
+        case 0:
+            dropBig.toggleClass('active');
+            drop1.removeClass('active');
+            drop2.removeClass('active');
+            drop3.removeClass('active');
+            break;
+
+            case 1:
+            drop1.toggleClass('active');
+            dropBig.removeClass('active');
+            drop2.removeClass('active');
+            drop3.removeClass('active');
+            break;
+
+            case 2:
+            drop2.toggleClass('active');
+            dropBig.removeClass('active');
+            drop3.removeClass('active');
+            drop1.removeClass('active');
+            break;
+
+            case 3:
+            drop3.toggleClass('active');
+            dropBig.removeClass('active');
+            drop1.removeClass('active');
+            drop2.removeClass('active');
+            break;
+    
+    }
+
+
+  } );
+      
+      dropBig.mouseleave( function() {
+
+        dropBig.removeClass('active');
+
+      } );
+
+      drop1.mouseleave( function() {
+
+        drop1.removeClass('active');
+
+      } );
+
+      drop2.mouseleave( function() {
+
+        drop2.removeClass('active');
+
+      } );
+
+      drop3.mouseleave( function() {
+
+        drop3.removeClass('active');
+
+      } );
+
+
+    // End ready
+} );
+
+
+
+
